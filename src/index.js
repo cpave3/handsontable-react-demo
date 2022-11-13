186f32a5ffbf5346001e6b3a1a5ce058cebf3780
cpave3 <cpave3@users.noreply.github.com>
2022-11-13 17:45:27 +1100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import 'handsontable/dist/handsontable.full.min.css';
 import App from './App';
 
+import Handsontable from 'handsontable/base';
+import { registerAllModules } from 'handsontable/registry';
+
+registerAllModules();
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
