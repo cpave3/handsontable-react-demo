edf0b063e50ac12225bf8ce1c9dc3242657bc198
cpave3 <cpave3@users.noreply.github.com>
2022-11-13 17:44:31 +1100

    2 files modified, 2 added

---
+++ src/DataGrid.js
@@ -0,0 +1,54 @@
+import { HotTable, HotColumn } from '@handsontable/react';
+
+import { data } from './data.js';
+
+export default function DataGrid() {
+  return (
+    <HotTable
+      data={data}
+      height={450}
+      colWidths={[140, 126, 192, 100, 100, 90, 90, 110, 97]}
+      colHeaders={[
+        'Company name',
+        'Country',
+        'Name',
+        'Sell date',
+        'Order ID',
+        'In stock',
+        'Qty',
+        'Progress',
+        'Rating',
+      ]}
+      dropdownMenu={true}
+      hiddenColumns={{
+        indicators: true,
+      }}
+      contextMenu={true}
+      multiColumnSorting={true}
+      filters={true}
+      rowHeaders={true}
+      afterGetColHeader={alignHeaders}
+      beforeRenderer={addClassesToRows}
+      afterGetRowHeader={drawCheckboxInRowHeaders}
+      afterOnCellMouseDown={changeCheckboxCell}
+      manualRowMove={true}
+      licenseKey="non-commercial-and-evaluation"
+    >
+      <HotColumn data={1} />
+      <HotColumn data={2} />
+      <HotColumn data={3} />
+      <HotColumn data={4} type="date" allowInvalid={false} />
+      <HotColumn data={5} />
+      <HotColumn data={6} type="checkbox" className="htCenter" />
+      <HotColumn data={7} type="numeric" />
+      <HotColumn data={8} readOnly={true} className="htMiddle">
+        {/* @ts-ignore Element inherits some props. It's hard to type it. */}
+        <ProgressBarRenderer hot-renderer />
+      </HotColumn>
+      <HotColumn data={9} readOnly={true} className="htCenter">
+        {/* @ts-ignore Element inherits some props. It's hard to type it. */}
+        <StarsRenderer hot-renderer />
+      </HotColumn>
+    </HotTable>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import 'handsontable/dist/handsontable.full.min.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
